f68d0b0fab43cd9cdeed879dfaeca598e05938a9
huanniangstudio <891863891@qq.com>
2022-11-18 17:09:27 +0800

    v1.0.1

---
+++ static/js/23.1ec4cf5e.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@uniswap/interface"]=this["webpackJsonp@uniswap/interface"]||[]).push([[23],{793:function(s,e){s.exports={messages:{}}}}]);
+//# sourceMappingURL=23.1ec4cf5e.chunk.js.map
--- static/js/24.efbbd37a.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@uniswap/interface"]=this["webpackJsonp@uniswap/interface"]||[]).push([[24],{793:function(s,e){s.exports={messages:{}}}}]);
-//# sourceMappingURL=24.efbbd37a.chunk.js.map
